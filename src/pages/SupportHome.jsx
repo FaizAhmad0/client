870671679e851c0470a57efed4d71a587cadc5ec
FaizAhmad0 <118246870+FaizAhmad0@users.noreply.github.com>
2024-09-20 10:25:59 +0530

    added media query

--- src/pages/SupportHome.jsx
+++ src/pages/SupportHome.jsx
@@ -223,6 +223,7 @@ const SupportHome = () => {
             <Button
               style={{
                 background: "#353979",
+                color:"white",
                 boxShadow:
                   "0 4px 8px 0 rgba(255, 255, 255, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
                 alignItems: "center",
